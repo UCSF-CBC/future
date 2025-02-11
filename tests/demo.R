e7d0f9e55565d5b8ff96ad292018400ff321a75d
Henrik Bengtsson <henrik.bengtsson@gmail.com>
2025-02-10 17:32:53 -0800

    Pass checks also when R_FUTURE_PLAN=multicore

--- tests/demo.R
+++ tests/demo.R
@@ -3,7 +3,10 @@ source("incl/start.R")
 message("*** Demos ...")
 
 message("*** Fibonacci demo of the 'future' package ...")
+# Temporarily protect against non-default R_FUTURE_PLAN
+oopts <- options(future.plan = NULL)
 demo("fibonacci", package = "future", ask = FALSE)
+options(oopts)
 message("*** Fibonacci demo of the 'future' package ... DONE")
 
 
